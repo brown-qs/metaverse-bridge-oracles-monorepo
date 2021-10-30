c58fe877dc6ac67ebb049a49519e6f9dbcbde6be
Mevan Abeydeera <mevan_ab@hotmail.com>
2021-10-30 23:03:24 +0400

    Add logout button

--- packages/minecraft-oracle-ui/src/components/Account/Account.tsx
+++ packages/minecraft-oracle-ui/src/components/Account/Account.tsx
@@ -1,3 +1,8 @@
+import React, { useState } from 'react';
+import Popover from '@mui/material/Popover';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
 import { Button } from 'ui';
 import { HeaderBalance } from 'components/HeaderBalance/HeaderBalance';
 import { UnsupportedChainIdError } from '@web3-react/core';
@@ -11,6 +16,7 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWalletSh
 import { useStyles } from './Account.styles';
 
 export const Account = () => {
+  const [ accountButton, setAccountButton ] = useState<HTMLButtonElement | null>(null);
   const { account, error } = useActiveWeb3React();
   const { setAccountDialogOpen } = useAccountDialog();
   const hideAddress = useMediaQuery(`(max-width: 500px)`);
@@ -19,9 +25,27 @@ export const Account = () => {
   const errMessage =
     error instanceof UnsupportedChainIdError ? 'Wrong Network' : 'Error';
 
-  const { button } = useStyles();
+  const { button, popoverListItem } = useStyles();
 
-  const {authData} = useAuth()
+  const { authData, setAuthData } = useAuth()
+
+    const handleAccountPopoverClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAccountButton(event.currentTarget);
+    };
+
+    const handleAccountPopoverClose = () => {
+        setAccountButton(null);
+    };
+
+    const handleLogout = () => {
+        setAuthData({
+            jwt: undefined,
+            userProfile: undefined
+        })
+    };
+
+    const open = Boolean(accountButton);
+    const id = open ? 'account-popover' : undefined;
 
   console.log({authData})
 
@@ -59,16 +83,43 @@ export const Account = () => {
           'Connect Wallet'
         )}
       </Button>
-      {!!authData && !!authData.userProfile && <Button
-        className={button}
-        size="medium"
-        onClick={() => setAccountDialogOpen(true)}
-      >
-        <div style={{ fontSize: 0, margin: '0 8px' }}>
-            <PersonSharpIcon />
-        </div>
-        {`${authData.userProfile.userName}`}
-      </Button>}
+      {!!authData && !!authData.userProfile &&
+      (
+          <>
+              <Button
+                className={button}
+                size="medium"
+                onClick={handleAccountPopoverClick}
+              >
+                <div style={{ fontSize: 0, margin: '0 8px' }}>
+                    <PersonSharpIcon />
+                </div>
+                {`${authData.userProfile.userName}`}
+              </Button>
+              <Popover
+                  id={id}
+                  open={open}
+                  anchorEl={accountButton}
+                  onClose={handleAccountPopoverClose}
+                  anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                  }}
+              >
+                  <List dense sx={{ width: '150px', bgcolor: '#111' }}>
+                      <ListItem className={popoverListItem} disablePadding>
+                          <ListItemButton onClick={handleLogout}>
+                              Logout
+                          </ListItemButton>
+                      </ListItem>
+                   </List>
+              </Popover>
+          </>
+      )}
     </>
   );
 };
