33c9808af6d71111fef1aa00117553fcf27a14e5
Mevan Abeydeera <mevan_ab@hotmail.com>
2021-10-31 19:36:04 +0400

    Remove toggle checkbox

--- packages/minecraft-oracle-ui/src/pages/profile/index.tsx
+++ packages/minecraft-oracle-ui/src/pages/profile/index.tsx
@@ -65,19 +65,6 @@ console.log('ONCHAIN', onChainResources);
 
     // const { jwt, userProfile } = authData;
 
-    const handleToggle = (value: string) => () => {
-        const currentIndex = checked.indexOf(value);
-        const newChecked = [...checked];
-
-        if (currentIndex === -1) {
-            newChecked.push(value);
-        } else {
-            newChecked.splice(currentIndex, 1);
-        }
-
-        setChecked(newChecked);
-    };
-
     const playerEligible = true;
 
     const {
