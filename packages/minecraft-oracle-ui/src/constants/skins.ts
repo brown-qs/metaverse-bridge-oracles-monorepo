b2c849c9e1f63cbdf3db06e2707f0ba403219bc0
James Wu <james@wubur.com>
2021-12-26 20:24:30 +0100

    feat: tooltips on ui

---
+++ packages/minecraft-oracle-ui/src/constants/skins.ts
@@ -0,0 +1,20 @@
+
+export type SkinLabels = {
+    [key: string]: any
+}
+
+export const SKIN_LABELS: SkinLabels = {
+    '0x0': {
+        '0': {
+            label: 'Default female skin'
+        },
+        '1': {
+            label: 'Default male skin'
+        },
+        label: 'Default skins'
+    },
+    '0xb654611f84a8dc429ba3cb4fda9fad236c505a1a': {
+        label: 'Moonsama skin. With fireproving and haste buff accessories.',
+        name: 'Moonsama'
+    }
+}
