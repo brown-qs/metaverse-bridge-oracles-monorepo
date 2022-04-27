fe8dad8a7563932893c5f9e6c5f8d75441a98a67
kyilkhor <kyilkhorsama@gmail.com>
2022-04-27 22:11:12 +0200

    fix: typo

--- packages/minecraft-oracle-api/src/provider/provider.module.ts
+++ packages/minecraft-oracle-api/src/provider/provider.module.ts
@@ -1,20 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ChainModule } from '../chain/chain.module';
 import {
     OracleWalletProvider,
     EvmChainClientProvider,
     MicrosoftSetupParamsProvider,
+    RecognizedAssetsProvider,
+    RecognizedChainAssetsProvider,
 } from '.';
+import { ContractsCallbackProvider } from './contract';
 
 @Module({
+    imports: [
+        ChainModule
+    ],
     providers: [
         OracleWalletProvider,
         EvmChainClientProvider,
-        MicrosoftSetupParamsProvider
+        MicrosoftSetupParamsProvider,
+        RecognizedChainAssetsProvider,
+        RecognizedAssetsProvider,
+        ContractsCallbackProvider
     ],
     exports: [
         OracleWalletProvider,
         EvmChainClientProvider,
-        MicrosoftSetupParamsProvider
+        MicrosoftSetupParamsProvider,
+        RecognizedChainAssetsProvider,
+        RecognizedAssetsProvider,
+        ContractsCallbackProvider
     ]
 })
 export class ProviderModule {}
