ed64b830f87e0fcc84f6d4afaa550e55a5795d8d
Ishan Ahuja <ishanahuja@gmail.com>
2022-05-25 13:33:22 +0530

    feat: mobile frenly moonsama customiser; downloads cors fixed; WIP continued

--- packages/minecraft-oracle-ui/src/ui/Navigation/NavMenuItem.tsx
+++ packages/minecraft-oracle-ui/src/ui/Navigation/NavMenuItem.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import "@fontsource/orbitron/500.css";
 import { useClasses } from 'hooks';
 
-export default function NavMenuItem({href, label, external=false}: {href: string, label: string, external?: boolean}) {
+export default function NavMenuItem({ href, label, external = false }: { href: string, label: string, external?: boolean }) {
   const styles = () => ({
     NavItem: {
       textTransform: 'uppercase',
@@ -38,17 +38,16 @@ export default function NavMenuItem({href, label, external=false}: {href: string
     NavItem,
   } = useClasses(styles);
 
+  return (
+    <Link href={href} className={LinkStyle} target={`${external ? '_blank' : '_self'}`}>
+      <Box className={NavItem}>
+        <span style={{ marginRight: 'auto' }}>{label}</span>
 
-return (
-  <Link href={href} className={LinkStyle} target={`${external ? '_blank' : '_self'}`}>
-    <Box className={NavItem}>
-      <span style={{marginRight: 'auto'}}>{label}</span>
-
-      {external && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{ marginLeft: '8px', flexShrink: 0 }} className={Chevron} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24">
-        <path stroke="none" d="M0 0h24v24H0z"></path>
-        <path d="M8 8h8v8"></path>
-      </svg>}
-    </Box>
-  </Link>
-)
+        {external && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{ marginLeft: '8px', flexShrink: 0 }} className={Chevron} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24">
+          <path stroke="none" d="M0 0h24v24H0z"></path>
+          <path d="M8 8h8v8"></path>
+        </svg>}
+      </Box>
+    </Link>
+  )
 }
