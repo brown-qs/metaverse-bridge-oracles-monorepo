9a8a17765361e4631b3cee189d9faee6b8907c44
kyilkhor <kyilkhorsama@gmail.com>
2022-05-29 00:21:30 +0200

    wtf

--- packages/minecraft-oracle-ui/src/pages/moonsama/designer/index.tsx
+++ packages/minecraft-oracle-ui/src/pages/moonsama/designer/index.tsx
@@ -983,7 +983,6 @@ const CharacterDesignerPage = ({ authData }: { authData: AuthData }) => {
             {(new URL(`/moonsama/customzier/${currentCustomization.parent?.chainId}/${currentCustomization.parent?.assetAddress}/${currentCustomization.parent?.assetId}`, `${window.location.protocol}//${window.location.host}`)).href}
           </Typography>
           }
-
           {!saveProgress.inProgress && saveProgress.errorMessage && <Typography id="modal-modal-description" sx={{ mt: 2, wordBreak: 'break-word', wordWrap: 'break-word', overflowWrap: 'break-word', textAlign: 'center' }}>
             {saveProgress.errorMessage}
           </Typography>
