b6a15d303bcb5eee7e6ca3b1205bd2750c99931e
mister jame <khunjamebkk@gmail.com>
2022-06-19 23:30:32 -0400

    Refined account pages. KILT working.

---
+++ packages/minecraft-oracle-ui/src/ui/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,47 @@
+import { Alert, AlertColor, CircularProgress, Stack } from "@mui/material"
+import { useState } from "react"
+
+export interface AuthLayoutProps {
+  title: string,
+  loading: boolean,
+  handleAlertClose?: () => void,
+  alert?: {
+    severity: string,
+    text: string,
+  }
+}
+
+export const AuthLayout = ({ children, ...props }: React.PropsWithChildren<AuthLayoutProps>) => {
+
+
+
+  return (
+    <Stack direction="column" alignItems='stretch' textAlign='center' spacing={2}>
+      {!!props?.alert && !props.loading
+        ?
+        <Stack direction="column" spacing={6} margin={2} alignContent='center' textAlign='center'>
+          <Alert severity={props.alert.severity as AlertColor} onClose={props.handleAlertClose}>{props.alert.text}</Alert>
+        </Stack>
+
+        :
+        <>
+          <Stack direction="column" alignContent='center' textAlign='center'>
+            <h1>{props.title}</h1>
+          </Stack>
+          {props.loading
+            ?
+            <Stack direction="row">
+              <CircularProgress />
+            </Stack>
+            :
+            <>
+              {children}
+            </>
+          }
+        </>
+      }
+
+    </Stack >
+  )
+}
+
