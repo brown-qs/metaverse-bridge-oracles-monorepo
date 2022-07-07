06552ef4ff60b069810e7be7e0fbef54bd53475c
James <khunjamebkk@gmail.com>
2022-07-07 14:55:07 +0700

    Kilt login should work in staging/production

--- packages/minecraft-oracle-ui/src/utils/kilt.ts
+++ packages/minecraft-oracle-ui/src/utils/kilt.ts
@@ -56,8 +56,9 @@ export async function walletLogin(extension: InjectedWindowProvider) {
 
   console.log("Starting wallet login...")
   const result = await fetch(`http://localhost:3030/api/v1/auth/kilt/wallet_login?sessionId=${sessionId}`);
-  const message = await result.json();
-
+  let message = await result.json();
+  //backwards compatibility with old sporran extensions, newer extensions switch from senderKeyId > senderKeyUri etc.
+  message = { ...message, senderKeyId: message.senderKeyUri, receiverKeyId: message.receiverKeyUri }
   console.log("sending wallet login to extension session...")
   console.log(message)
   try {
@@ -69,13 +70,16 @@ export async function walletLogin(extension: InjectedWindowProvider) {
 
 
   return await new Promise((resolve, reject) => {
-    session.listen(async message => {
+    session.listen(async sporranMessage => {
       console.log("extension session responded with message...")
+      console.log(message)
+
+      let sMessage = { ...sporranMessage, receiverKeyUri: sporranMessage.receiverKeyId, senderKeyUri: sporranMessage.senderKeyId }
       try {
         const loginResult = await fetch(`http://localhost:3030/api/v1/auth/kilt/wallet_login`, {
           method: 'POST',
           headers: { "Content-Type": 'application/json' },
-          body: JSON.stringify({ sessionId, message }),
+          body: JSON.stringify({ sessionId, message: sMessage }),
         });
         const json = await loginResult.json()
         resolve(json)
