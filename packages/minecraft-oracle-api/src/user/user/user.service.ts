5f05445bbeb334dc473920b62ac583e578f0c55c
James <khunjamebkk@gmail.com>
2022-07-10 19:12:38 +0700

    Good progress on user merge

--- packages/minecraft-oracle-api/src/user/user/user.service.ts
+++ packages/minecraft-oracle-api/src/user/user/user.service.ts
@@ -1,4 +1,5 @@
 import { InjectConnection, InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import { Like } from "typeorm"
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Connection, DeepPartial, EntityManager, FindConditions, FindManyOptions, FindOneOptions, ObjectID, Repository, UpdateResult } from 'typeorm';
@@ -20,6 +21,8 @@ import { PlayerScoreEntity } from 'src/playerscore/playerscore.entity';
 import { WINSTON_MODULE_NEST_PROVIDER, WinstonLogger } from 'nest-winston';
 import { MinecraftLinkEntity } from '../minecraft-link/minecraft-link.entity';
 import { MinecraftLinkEvent } from 'src/common/enums/MinecraftLinkEvent';
+import { ResourceInventoryOffsetEntity } from 'src/resourceinventoryoffset/resourceinventoryoffset.entity';
+import { UserRole } from 'src/common/enums/UserRole';
 @Injectable()
 export class UserService {
     context: string;
@@ -150,6 +153,8 @@ export class UserService {
                     const mcLink = queryRunner.manager.create(MinecraftLinkEntity, { minecraftUuid, user: existingMinecraft, initiator: newUser, event: MinecraftLinkEvent.UNLINK })
                     await queryRunner.manager.save(MinecraftLinkEntity, mcLink)
                     queryRunner.manager.create(MinecraftLinkEntity, { minecraftUuid, user: existingMinecraft, initiator: newUser, event: MinecraftLinkEvent.UNLINK })
+
+
                     //minecraft user has never been migrated
                 } else {
                     //safety check, uuid and minecraftUuid should be same for unmigrated minecraft users
@@ -159,16 +164,132 @@ export class UserService {
                     this.logger.debug(`user.service::linkMinecraftByUserUuid userUuid: ${userUuid} minecraftUuid: ${minecraftUuid}, MC account has never been migrated to email before, moving relationships`, this.context)
 
                     //move all relationships to email user
-                    const relationships = [{ entity: AssetEntity, fk: "owner" }, { entity: SummonEntity, fk: "owner" }, { entity: SnapshotItemEntity, fk: "owner" }, { entity: SnaplogEntity, fk: "owner" }, { entity: InventoryEntity, fk: "owner" }, { entity: SkinEntity, fk: "owner" }, { entity: ResourceInventoryEntity, fk: "owner" }, { entity: PlaySessionEntity, fk: "player" }, { entity: PlayerAchievementEntity, fk: "player" }, { entity: PlayerGameItemEntity, fk: "player" }, { entity: PlayerScoreEntity, fk: "player" }]
+                    const relationships = [
+                        { entity: AssetEntity, fk: "owner", },
+                        { entity: SummonEntity, fk: "owner" },
+                        { entity: SnapshotItemEntity, fk: "owner" },
+                        { entity: SnaplogEntity, fk: "owner" },
+                        { entity: InventoryEntity, fk: "owner" },
+                        { entity: SkinEntity, fk: "owner" },
+                        { entity: ResourceInventoryEntity, fk: "owner" },
+                        { entity: SnapshotItemEntity, fk: "owner" },
+
+                        { entity: PlaySessionEntity, fk: "player" },
+                        { entity: PlayerAchievementEntity, fk: "player" },
+                        { entity: PlayerGameItemEntity, fk: "player" },
+                        { entity: PlayerScoreEntity, fk: "player" },
+                    ]
                     for (const { entity, fk } of relationships) {
                         await queryRunner.manager.update(entity, { [fk]: minecraftUuid }, { [fk]: userUuid })
                     }
 
+                    //rewrite ids
+                    //ids look like: 30828468962f44e681190c1f6d82d42a-1285-0x1b30a3b5744e733d8d2f19f0812e3f79152a8777-14 ~ need to be written with new uuid
+                    //ResourceInventoryOffsetEntity has a fk that references ResourceInventoryEntity, we need update cascade for this
+                    //we don't need to cacade update user ids, because we are moving from the old mc user which still exists to the email user which exists, the mc user is only deleted after all the relationships have been moved
+                    const rewriteIdEntities = [ResourceInventoryEntity, ResourceInventoryOffsetEntity, SkinEntity]
+                    for (const entity of rewriteIdEntities) {
+
+                        let relations = ["owner"]
+                        if (entity === ResourceInventoryOffsetEntity) {
+                            relations = []
+                        }
+                        console.log(`Entity: ${entity}`)
+                        const rows = await queryRunner.manager.find(entity, { where: { id: Like(`${minecraftUuid}-%`) }, relations })
+                        //console.log(`Rows: ${}`)
+                        for (const row of rows) {
+                            const oldRowId = row.id
+                            const newRowId = oldRowId.replace(`${minecraftUuid}-`, `${userUuid}-`)
+                            //sanity check, make sure query returned correct result
+                            if (!oldRowId.startsWith(`${minecraftUuid}-`)) {
+                                throw new Error("Old minecraft uuid not in id")
+                            }
+
+                            //sanity check: owner should already be updated to userUuid from above
+                            // console.log(`row.owner: ${JSON.stringify(row.owner)}`)
+                            if (entity !== ResourceInventoryOffsetEntity && row.owner.uuid !== userUuid) {
+                                throw new Error("Changing composite id to new user uuid but owner is a different user")
+                            }
+
+                            //if two mc accounts are merged in, will have multiple default skins, could have multiple exo skins, etc.  
+                            if (entity === SkinEntity) {
+                                //on first skin row
+                                if (rows.indexOf(row) === 0) {
+                                    const emailUserEquipped = !!await queryRunner.manager.findOne(SkinEntity, { where: { id: Like(`${userUuid}-%`), equipped: true } })
+                                    const userToMergeInIsEquipped = (rows as any).some((row: SkinEntity) => row.equipped === true)
+                                    console.log(`emailUserEquipped: ${emailUserEquipped}, userToMergeInIsEquipped: ${userToMergeInIsEquipped}`)
+                                    if (emailUserEquipped && userToMergeInIsEquipped) {
+                                        console.log("Users who are being merged both are equipped with skins, will un-equip email user and will use equip from user who is being merged in")
+                                        await queryRunner.manager.update(SkinEntity, { id: Like(`${userUuid}-%`) }, { equipped: false })
+                                    }
+                                }
+                                //delete skin with this id from email user, otherwise updating the old row to new row will cause primary key constraint error
+                                await queryRunner.manager.delete(SkinEntity, { id: newRowId })
+
+                            }
+
+                            await queryRunner.manager.update(entity, { "id": oldRowId }, { "id": newRowId })
+
+
+                        }
+                    }
+
+                    //add bait
+                    const emailUserBait = await queryRunner.manager.findOne(ResourceInventoryEntity, { where: { id: Like(`${userUuid}-%`) } })
+                    const mcUserBait = await queryRunner.manager.findOne(ResourceInventoryEntity, { where: { id: Like(`${minecraftUuid}-%`) } })
+
+                    if (!!emailUserBait && !!mcUserBait) {
+                        console.log("both users have bait, needs to be added...")
+                        console.log(JSON.stringify(mcUserBait))
+                    }
+                    throw new Error("Stop here")
+                    /*
+        // TODO fixme
+        const bait = await this.resourceInventoryService.findOne({ owner: user }, { relations: ['owner', 'offset'] })
+        if (!!bait) {
+            const baitAsset = userAssets.find(x => x.assetId === bait.assetId && x.collectionFragment.recognizedAssetType.valueOf() === RecognizedAssetType.RESOURCE.valueOf())
+
+            if (!!baitAsset) {
+                assets.push(
+                    {
+                        amount: formatEther(BigNumber.from(bait.amount).sub(bait.offset?.amount ?? '0')),
+                        assetAddress: baitAsset.collectionFragment.collection.assetAddress.toLowerCase(),
+                        assetType: baitAsset.collectionFragment.collection.assetType,
+                        assetId: baitAsset.assetId,
+                        name: baitAsset.collectionFragment.name,
+                        exportable: !baitAsset.enraptured,
+                        hash: baitAsset.hash,
+                        summonable: false,
+                        recognizedAssetType: baitAsset.recognizedAssetType.valueOf(),
+                        enraptured: baitAsset.enraptured,
+                        exportChainId: baitAsset.collectionFragment.collection.chainId,
+                        exportAddress: baitAsset.assetOwner?.toLowerCase(),
+                    }
+                )
+            }
+        }
+                    */
+
+                    //     throw new Error("don't continue")
                     //move
                     //move other fields to email user
-                    await queryRunner.manager.update(UserEntity, { uuid: userUuid }, { role: existingMinecraft.role, allowedToPlay: existingMinecraft.allowedToPlay, blacklisted: existingMinecraft.blacklisted, vip: existingMinecraft.vip, numGamePassAsset: existingMinecraft.numGamePassAsset, serverId: existingMinecraft.serverId, preferredServer: existingMinecraft.preferredServer, lastUsedAddress: existingMinecraft.lastUsedAddress, usedAddresses: existingMinecraft.usedAddresses })
+                    const emailUser = await queryRunner.manager.findOne(UserEntity, { uuid: userUuid })
 
-                    //TODO: gganbu
+                    const allowedToPlay = emailUser?.allowedToPlay === true || existingMinecraft?.allowedToPlay === true
+                    const blacklisted = emailUser?.blacklisted === true || existingMinecraft?.blacklisted === true
+                    const vip = emailUser?.vip === true || existingMinecraft?.vip === true
+                    const numGamePassAsset = emailUser?.numGamePassAsset ?? 0 + existingMinecraft?.numGamePassAsset ?? 0
+                    let role = numGamePassAsset > 0 ? UserRole.PLAYER : UserRole.NONE
+                    let usedAddresses = [...emailUser?.usedAddresses ?? [], ...existingMinecraft?.usedAddresses ?? []]
+                    //remove duplicates
+                    usedAddresses = usedAddresses.filter((item, i, self) => self.lastIndexOf(item) === i)
+                    //if either of the two users being merged is an admin, user is admin
+                    if (emailUser?.role.valueOf() === UserRole.ADMIN.valueOf() || existingMinecraft?.role.valueOf() === UserRole.ADMIN.valueOf()) {
+                        role = UserRole.ADMIN
+                    }
+
+                    await queryRunner.manager.update(UserEntity, { uuid: userUuid }, { role, allowedToPlay, blacklisted, vip, numGamePassAsset, serverId: existingMinecraft.serverId, preferredServer: existingMinecraft.preferredServer, lastUsedAddress: existingMinecraft.lastUsedAddress, usedAddresses })
+
 
                     //remove old minecraft user
                     await queryRunner.manager.remove(UserEntity, existingMinecraft)
@@ -176,10 +297,10 @@ export class UserService {
             }
 
             await queryRunner.manager.update(UserEntity, { uuid: userUuid }, { minecraftUuid: minecraftUuid, minecraftUserName, hasGame })
-            //TODO: add log of minecraft link/unlinks + relationship moves
             await queryRunner.commitTransaction();
 
         } catch (e) {
+            console.log(e.stack)
             err = e
             this.logger.error(`user.service::linkMinecraftByUserUuid, error linking minecraft account ${e}`, e, this.context)
             // since we have errors lets rollback the changes we made
