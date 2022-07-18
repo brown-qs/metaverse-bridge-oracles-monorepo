4978c9c4f343fbce9a5114695e8e11d7581afe1d
James <khunjamebkk@gmail.com>
2022-07-18 13:59:13 +0700

    Bait migrations properly working all cases

--- packages/minecraft-oracle-api/src/views/user-bait.view.ts
+++ packages/minecraft-oracle-api/src/views/user-bait.view.ts
@@ -13,12 +13,20 @@ import { ViewEntity, Connection, ViewColumn, Column } from "typeorm";
 
         //add limit so view is not editable
         //https://stackoverflow.com/questions/58026241/how-to-create-a-read-only-view-in-postgresql-similar-to-oracle
+
+        const amountDecimal = "CAST(resource_inventory.amount as NUMERIC(78))/1e18"
+        const offsetDecimal = "CAST(resource_inventory_offset.amount as NUMERIC(78))/1e18"
         return connection.createQueryBuilder()
             .addSelect("resource_inventory.ownerUuid", "uuid")
             .addSelect("resource_inventory.amount", "amount")
             .addSelect("resource_inventory_offset.amount", "offset")
-            .addSelect("")
+            .addSelect(amountDecimal, "amountDecimal")
+            .addSelect(offsetDecimal, "offsetDecimal")
+            .addSelect(`${amountDecimal}-COALESCE(${offsetDecimal},0)`, "balance")
+            .addSelect("user.email", "email")
+            .addSelect("user.minecraftUserName", "minecraftUserName")
             .from(ResourceInventoryEntity, "resource_inventory")
+            .where(`resource_inventory.assetId = '14'`)
             .leftJoin(UserEntity, "user", "user.uuid = resource_inventory.ownerUuid")
             .leftJoin(ResourceInventoryOffsetEntity, "resource_inventory_offset", "resource_inventory.id = resource_inventory_offset.resourceInventoryId")
             .orderBy("resource_inventory.ownerUuid", "ASC")
