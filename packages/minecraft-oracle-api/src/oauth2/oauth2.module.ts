e84ad742b771d8430d95d8dbf365b0f254b6462f
James <khunjamebkk@gmail.com>
2022-07-20 21:13:46 +0700

    Authorize endpoint complete

--- packages/minecraft-oracle-api/src/oauth2/oauth2.module.ts
+++ packages/minecraft-oracle-api/src/oauth2/oauth2.module.ts
@@ -3,19 +3,9 @@ import { Oauth2AuthorizationModule } from './oauth2-authorization/oauth2-authori
 import { Oauth2ClientModule } from './oauth2-client/oauth2-client.module';
 import { Oauth2ResourceModule } from './oauth2-resource/oauth2-resource.module';
 import OAuth2Server from 'oauth2-server'
-import { OAUTH2_SERVER } from './constants';
-import { Oauth2Service } from './oauth2.service';
 @Module({
-    imports: [Oauth2ClientModule],
-    providers: [{
-        provide: OAUTH2_SERVER,
-        useFactory: (model: Oauth2Service): OAuth2Server => {
-
-            return new OAuth2Server({ model })
-        },
-        inject: [Oauth2Service],
-    }, Oauth2Service],
-    exports: [OAUTH2_SERVER],
+    imports: [Oauth2ClientModule, Oauth2AuthorizationModule, Oauth2ResourceModule],
+    exports: [],
     controllers: []
 })
 export class Oauth2Module { }
