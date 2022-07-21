ae34b7949b799689b7ea98c18382bc5016d3d9bc
James <khunjamebkk@gmail.com>
2022-07-21 16:10:49 +0700

    Oauth front end half assed for now

---
+++ packages/minecraft-oracle-ui/src/pages/oauth/index.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { AuthLayout, Loader } from 'ui';
+import { useAuth, useClasses } from 'hooks';
+import Tooltip from '@mui/material/Tooltip';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import WhiteLogo from 'assets/images/moonsama-glitch-white.svg';
+import LeftImage from 'assets/images/home/left.png';
+import RightImageFlip from 'assets/images/home/right.png';
+import Box from '@mui/material/Box';
+import "@fontsource/orbitron/500.css";
+import { Alert, AlertColor, Avatar, Button, Card, CardContent, CardHeader, Chip, CircularProgress, Collapse, IconButton, List, ListItem, ListItemAvatar, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, useMediaQuery } from '@mui/material';
+import { theme } from 'theme/Theme';
+import { Edit, SportsEsports, ExpandLess, ExpandMore, StarBorder } from '@mui/icons-material';
+import PersonIcon from '@mui/icons-material/Person';
+import { Redirect } from 'react-router-dom';
+import axios, { AxiosError } from 'axios';
+const OauthPage = () => {
+  const { authData, setAuthData } = useAuth();
+  const [isLoading, setIsLoading] = useState(true)
+  const [failureMessage, setFailureMessage] = useState("")
+  let history = useHistory();
+  const { search } = useLocation()
+
+  const getOauthCode = async () => {
+    setIsLoading(true)
+    try {
+      const result = await axios({
+        method: 'get',
+        url: `${process.env.REACT_APP_BACKEND_API_URL}/oauth2/authorize`,
+        headers: {
+          "Authorization": `Bearer ${authData?.jwt}`,
+          "Content-Type": "application/json"
+        },
+        params: Object.fromEntries(new URLSearchParams(search))
+      });
+      window.location.href = result.data.url
+    } catch (e) {
+      const err = e as AxiosError;
+
+      if (err?.response?.data.statusCode === 401) {
+        window.localStorage.removeItem('authData');
+        setAuthData(undefined);
+      } else if (!!err.response?.data?.message) {
+        setFailureMessage(`Error: ${String(err.response?.data?.message)}`)
+      } else {
+        setFailureMessage(String(e))
+      }
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getOauthCode()
+  }, [])
+
+
+  const handleAlertClose = () => {
+    setFailureMessage("")
+    getOauthCode()
+  }
+
+
+  let alert
+  if (failureMessage) {
+    alert = { severity: "error", text: failureMessage }
+  }
+  return (
+    <AuthLayout title="OAUTH" loading={isLoading} alert={alert} handleAlertClose={handleAlertClose}> </AuthLayout >
+  );
+}
+export default OauthPage;
