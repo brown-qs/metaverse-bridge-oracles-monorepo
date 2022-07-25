feb91cd44c7644389b9d7fc9bcd3fa8e92d01016
James Wu <james@wubur.com>
2022-07-25 15:27:47 +0200

    feat: moonsama 2.0 textures skins

--- packages/minecraft-oracle-api/scripts/texture.seed.ts
+++ packages/minecraft-oracle-api/scripts/texture.seed.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs'
+
 
 import { MaterialEntity } from '../src/material/material.entity'
-import { Connection, createConnection, getConnection } from 'typeorm'
+import { Connection, createConnection, getConnection, IsNull, Not } from 'typeorm'
 
 import { config } from 'dotenv'
 import { SnapshotItemEntity } from '../src/snapshot/snapshotItem.entity'
@@ -12,9 +12,42 @@ import { SummonEntity } from '../src/summon/summon.entity'
 import { InventoryEntity } from '../src/playerinventory/inventory.entity'
 import { PlaySessionEntity } from '../src/playsession/playsession.entity'
 import { PlaySessionStatEntity } from '../src/playsession/playsessionstat.entity'
-import { stringToStringAssetType } from '../src/utils'
-import { TextureType } from '../src/texture/texturetype.enum'
 import { SkinEntity } from '../src/skin/skin.entity'
+import { PlaySessionStatService } from '../src/playsession/playsessionstat.service'
+import { GameEntity } from '../src/game/game.entity'
+import { GameTypeEntity } from '../src/gametype/gametype.entity'
+import { AchievementEntity } from '../src/achievement/achievement.entity'
+import { PlayerAchievementEntity } from '../src/playerachievement/playerachievement.entity'
+import { PlayerScoreEntity } from '../src/playerscore/playerscore.entity'
+import { GganbuEntity } from '../src/gganbu/gganbu.entity'
+import { SnaplogEntity } from '../src/snaplog/snaplog.entity'
+import { RecognizedAssetType } from '../src/config/constants'
+import { ChainEntity } from '../src/chain/chain.entity'
+import { CollectionEntity } from '../src/collection/collection.entity'
+import { CollectionFragmentEntity } from '../src/collectionfragment/collectionfragment.entity'
+import { CompositeAssetEntity } from '../src/compositeasset/compositeasset.entity'
+import { CompositeCollectionFragmentEntity } from '../src/compositecollectionfragment/compositecollectionfragment.entity'
+import { CompositePartEntity } from '../src/compositepart/compositepart.entity'
+import { GameItemTypeEntity } from '../src/gameitemtype/gameitemtype.entity'
+import { GameScoreTypeEntity } from '../src/gamescoretype/gamescoretype.entity'
+import { PlayerGameItemEntity } from '../src/playergameitem/playergameitem.entity'
+import { ResourceInventoryEntity } from '../src/resourceinventory/resourceinventory.entity'
+import { ResourceInventoryOffsetEntity } from '../src/resourceinventoryoffset/resourceinventoryoffset.entity'
+import { SecretEntity } from '../src/secret/secret.entity'
+import { SyntheticItemEntity } from '../src/syntheticitem/syntheticitem.entity'
+import { SyntheticPartEntity } from '../src/syntheticpart/syntheticpart.entity'
+import { DidEntity } from '../src/user/did/did.entity'
+import { EmailChangeEntity } from '../src/user/email-change/email-change.entity'
+import { EmailLoginKeyEntity } from '../src/user/email-login-key/email-login-key.entity'
+import { EmailEntity } from '../src/user/email/email.entity'
+import { KiltDappEntity } from '../src/user/kilt-dapp/kilt-dapp.entity'
+import { KiltSessionEntity } from '../src/user/kilt-session/kilt-session.entity'
+import { MinecraftLinkEntity } from '../src/user/minecraft-link/minecraft-link.entity'
+import { MinecraftUserNameEntity } from '../src/user/minecraft-user-name/minecraft-user-name.entity'
+import { MinecraftUuidEntity } from '../src/user/minecraft-uuid/minecraft-uuid.entity'
+import { UserRole } from '../src/common/enums/UserRole'
+
+import * as fs from 'fs'
 
 config()
 
@@ -30,8 +63,49 @@ async function main() {
             host: process.env.TYPEORM_HOST,
             port: Number.parseInt(process.env.TYPEORM_PORT),
             database: process.env.TYPEORM_DATABASE,
-            entities: [MaterialEntity, SnapshotItemEntity, UserEntity, TextureEntity, AssetEntity, SummonEntity, InventoryEntity, PlaySessionEntity, PlaySessionStatEntity, SkinEntity],
-            synchronize: true
+            entities: [
+                UserEntity,
+                SnapshotItemEntity,
+                InventoryEntity,
+                TextureEntity,
+                SkinEntity,
+                PlayerScoreEntity,
+                MaterialEntity,
+                GameEntity,
+                GameTypeEntity,
+                AchievementEntity,
+                PlayerAchievementEntity,
+                SecretEntity,
+                AssetEntity,
+                SummonEntity,
+                PlaySessionEntity,
+                PlaySessionStatEntity,
+                GganbuEntity,
+                SnaplogEntity,
+                GameItemTypeEntity,
+                PlayerGameItemEntity,
+                GameScoreTypeEntity,
+                ChainEntity,
+                CollectionEntity,
+                CollectionFragmentEntity,
+                CompositeCollectionFragmentEntity,
+                CompositeAssetEntity,
+                CompositePartEntity,
+                SyntheticPartEntity,
+                SyntheticItemEntity,
+                ResourceInventoryEntity,
+                ResourceInventoryOffsetEntity,
+                EmailEntity,
+                KiltSessionEntity,
+                KiltDappEntity,
+                DidEntity,
+                EmailChangeEntity,
+                EmailLoginKeyEntity,
+                MinecraftLinkEntity,
+                MinecraftUserNameEntity,
+                MinecraftUuidEntity
+            ],
+            synchronize: false
         })
     } catch (err) {
         connection = getConnection('materialseeder')
@@ -41,44 +115,33 @@ async function main() {
         connection = await connection.connect()
     }
 
-    const materials = fs.readFileSync(__dirname + '/textures.csv').toString().split("\n").slice(1);
 
-    const jobs = materials.map(async (material) => {
-        if (!material) {
-            return undefined
-        }
-        const fragments = material.split(',')
-        if (!fragments || fragments.length == 0) {
-            return undefined
-        }
-        const entity: TextureEntity = {
-            assetType: stringToStringAssetType(fragments[0]),
-            assetAddress: fragments[1].slice(1).replace('"', ""),
-            assetId: fragments[2].slice(1).replace('"', ""),
-            accessories: fragments[3].slice(1).replace('"', "").slice(1).replace('}', "").split('|'),
-            type: TextureType.SKIN,
-            textureData: fragments[5],
-            textureSignature: fragments[6],
-            auction: fragments[8] === 'true'
-        }
-        try {
-            const e = connection.manager.create<TextureEntity>(TextureEntity, entity)
-            const success = await connection.manager.save<TextureEntity>(e)
-            console.log(!!success)
-        } catch (err) {
-            console.log(err)
-        }
-        return undefined
-    })
+    const materials = fs.readFileSync(__dirname + '/skin-list.csv').toString().split("\n").slice(1);
 
-    await Promise.all(jobs)
+    let itercounter = 1
+    const assetAddress = '0xb654611f84a8dc429ba3cb4fda9fad236c505a1a'
+    for (const line of materials) {
+        const fragments = line.split(',')
+        const assetId = fragments[0].split('.png')[0]
+        console.log(fragments[0].split('.png')[0])
 
-    //const entities = await connection.manager.find<TextureEntity>(TextureEntity, {relations: ['snapshots']})
+        const textureData = fragments[3]
+        const textureSignature = fragments[4]
 
-    const entities = await connection.manager.getRepository(TextureEntity).find({ auction: true })
-    console.log(entities?.[0])
+        //console.log( textureData, textureSignature)
+
+        const texture = await connection.manager.getRepository(TextureEntity).findOne({ where: {assetAddress, assetId }})
+
+        await connection.manager.getRepository(TextureEntity).update(texture, { textureData, textureSignature})
+    }
     await connection.close()
 }
 
 
 main()
+
+
+
+
+
+
