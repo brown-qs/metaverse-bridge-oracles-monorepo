271361b52f2fca928d2298fc3c502774fa0392d4
James <khunjamebkk@gmail.com>
2022-07-26 22:26:13 +0700

    Begin chakra rewrite

--- packages/minecraft-oracle-ui/src/components/Layout/Layout.tsx
+++ packages/minecraft-oracle-ui/src/components/Layout/Layout.tsx
@@ -1,14 +1,14 @@
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import { Box } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
 import MoonsamaNav from 'ui/Navigation/MoonsamaNav';
 
 import { LayoutProps } from './Layout.types';
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-      <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-        <MoonsamaNav/>
-        <Container style={{paddingLeft: 0, paddingRight: 0, flex: 1}} maxWidth={false}>{children}</Container>
-      </Box>
+    <Box minHeight="100vh" display="flex" flexDirection="column">
+      <MoonsamaNav />
+      <Container paddingLeft="0" paddingRight="0" flex="1" maxWidth="false">{children}</Container>
+    </Box>
   )
 };
