5a0866645bd6e4ec94b06a218d90d735821d5613
James <khunjamebkk@gmail.com>
2022-07-29 02:55:31 +0700

    Bridge stuff

--- packages/minecraft-oracle-ui/src/ui/Navigation/WalletAccount.tsx
+++ packages/minecraft-oracle-ui/src/ui/Navigation/WalletAccount.tsx
@@ -5,6 +5,7 @@ import { truncateAddress } from 'utils';
 import { useClasses } from 'hooks';
 import Identicon from 'components/Identicon/Identicon';
 import { Box } from '@chakra-ui/react';
+import { Wallet } from 'tabler-icons-react';
 
 export default function WalletAccount() {
   const { account } = useActiveWeb3React();
@@ -49,11 +50,7 @@ export default function WalletAccount() {
           </Box >
         ) : (
           <Box onClick={() => setAccountDialogOpen(true)} className={WalletDetailsStyle}>
-            <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '8px' }} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#0EEBA8" fill="none" strokeLinecap="round" strokeLinejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M17 8v-3a1 1 0 0 0 -1 -1h-10a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1 -1 1h-12a2 2 0 0 1 -2 -2v-12" />
-              <path d="M20 12v4h-4a2 2 0 0 1 0 -4h4" />
-            </svg>
+            <Wallet />
             <span>Connect Wallet</span>
           </Box>
         )}
