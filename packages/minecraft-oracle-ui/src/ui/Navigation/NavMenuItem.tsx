ad816981f6c4921489002a4a94adb8803cb6ed16
James <khunjamebkk@gmail.com>
2022-07-29 02:55:31 +0700

    Bridge stuff

--- packages/minecraft-oracle-ui/src/ui/Navigation/NavMenuItem.tsx
+++ packages/minecraft-oracle-ui/src/ui/Navigation/NavMenuItem.tsx
@@ -5,7 +5,7 @@ import { useClasses } from 'hooks';
 import { Brush, ChevronUpRight } from 'tabler-icons-react';
 export default function NavMenuItem({ href, label, external = false }: { href: string, label: string, external?: boolean }) {
   return (
-    <Link sx={{ textTransform: "uppercase", letterSpacing: "0.0032em", fontSize: "12px", color: "white", "&:hover": { color: "#F7CB49" } }} href={href} target={`${external ? '_blank' : '_self'}`}>
+    <Link _hover={{ "textDecoration": "none" }} sx={{ textTransform: "uppercase", letterSpacing: "0.0032em", fontSize: "12px", color: "white", "&:hover": { color: "#F7CB49" } }} href={href} target={`${external ? '_blank' : '_self'}`}>
       <HStack>
         <span style={{ marginRight: 'auto' }}>{label}</span>
 
