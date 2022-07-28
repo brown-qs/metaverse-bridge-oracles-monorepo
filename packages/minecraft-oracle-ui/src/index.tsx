b58d49e26059060ccdffbef563dc430f5dad1314
James <khunjamebkk@gmail.com>
2022-07-28 16:09:30 +0700

    Customizer has mui removed but not working

--- packages/minecraft-oracle-ui/src/index.tsx
+++ packages/minecraft-oracle-ui/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from "react-dom/client"
 import { initalizeValidation } from './utils/validation';
 import { ChakraProvider } from '@chakra-ui/react';
+import chakraTheme from './theme/chakra-theme';
 
 initalizeValidation();
 
@@ -11,7 +12,7 @@ const rootElem = document.getElementById("root")
 const root = createRoot(rootElem!);
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={chakraTheme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
