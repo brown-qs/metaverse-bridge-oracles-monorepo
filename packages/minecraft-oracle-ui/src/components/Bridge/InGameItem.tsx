d875f87a0e0e6ad7efaabd3e1c7b6c4f823f1cff
James <khunjamebkk@gmail.com>
2022-07-31 23:31:32 +0700

    Work on modals

--- packages/minecraft-oracle-ui/src/components/Bridge/InGameItem.tsx
+++ packages/minecraft-oracle-ui/src/components/Bridge/InGameItem.tsx
@@ -1,20 +1,33 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, Checkbox, HStack } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { InGameItemWithStatic } from "../../hooks/multiverse/useInGameItems";
 
 export const InGameItem: React.FC<{ data: InGameItemWithStatic, onClick?: () => void }> = ({ data, onClick }) => {
     return (
-        <HStack height="80px" width="100%" onClick={onClick} cursor="pointer" spacing="0" color="white" fontFamily="Rubik" fontSize="16px" _hover={{ color: "#FCD14E" }}>
+        <HStack height="80px" width="100%" spacing="0" color="white" fontFamily="Rubik" fontSize="16px" _hover={{ color: "#FCD14E" }}>
             <Box
+                cursor="pointer"
                 height="80px"
                 width="80px"
                 backgroundImage={`url(${data?.meta?.image})`}
                 backgroundRepeat="no-repeat"
                 backgroundSize="contain"
-                backgroundPosition="center">
+                backgroundPosition="center"
+                onClick={onClick}
+            >
             </Box>
-            <Box paddingLeft="8px">{data?.meta?.name ?? `${data.assetAddress} ${data.assetId}`}</Box>
-            <Box></Box>
+            <Box
+                cursor="pointer"
+                onClick={onClick}
+                flex="1"
+                paddingLeft="8px">
+                {data?.meta?.name ?? `${data.assetAddress} ${data.assetId}`}
+            </Box>
+            <Box
+                cursor="default"
+            >
+                <Checkbox></Checkbox>
+            </Box>
         </HStack>
     )
 };
