ea1a269d95b9957f5773ba5c6b2dd9217fb1a7cb
James <khunjamebkk@gmail.com>
2022-08-03 21:02:10 +0700

    Modal blur

--- packages/minecraft-oracle-ui/src/theme/components/modal.ts
+++ packages/minecraft-oracle-ui/src/theme/components/modal.ts
@@ -7,6 +7,10 @@ const Modal: ComponentStyleConfig = {
             color: "white",
             bg: "linear-gradient(311.18deg, #1A202C 67.03%, #4A5568 100%)",
             boxShadow: "0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
+        },
+        overlay: {
+            background: "rgba(0, 0, 0, 0.64)",
+            backdropFilter: "blur(8px)"
         }
     }
 }
