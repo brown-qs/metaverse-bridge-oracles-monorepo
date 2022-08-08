20d45ae505b9f1c3595312ba487479ad8245fdff
James <khunjamebkk@gmail.com>
2022-08-08 12:34:30 +0700

    Main page login

--- packages/minecraft-oracle-ui/src/state/index.ts
+++ packages/minecraft-oracle-ui/src/state/index.ts
@@ -1,6 +1,7 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query'
 import { save, load } from 'redux-localstorage-simple';
-
+import { bridgeApi } from './api/bridgeApi';
 import application from './application/reducer';
 import { updateVersion } from './global/actions';
 import transactions from './transactions/reducer';
@@ -11,15 +12,16 @@ const store = configureStore({
   reducer: {
     application,
     transactions,
+    [bridgeApi.reducerPath]: bridgeApi.reducer
   },
-  middleware: [
-    ...getDefaultMiddleware({ thunk: false }),
-    save({ states: PERSISTED_KEYS }),
-  ],
+  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware({ thunk: false }), bridgeApi.middleware, save({ states: PERSISTED_KEYS })],
   preloadedState: load({ states: PERSISTED_KEYS }),
 });
-
 store.dispatch(updateVersion());
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch)
 
 export default store;
 
