319bcab29961663f70a4eef3a025b1f92b27450f
brown-qs <neoprince1992@gmail.com>
2022-08-12 21:03:49 +0700

    Yarn upgrade complete

--- packages/minecraft-oracle-ui/src/index.tsx
+++ packages/minecraft-oracle-ui/src/index.tsx
@@ -3,11 +3,10 @@ import { createRoot } from "react-dom/client"
 import { initalizeValidation } from './utils/validation';
 import { ChakraProvider } from '@chakra-ui/react';
 import chakraTheme from './theme';
-
+import App from './app';
 initalizeValidation();
 
-// eslint-disable-next-line import/first
-import App from './app';
+
 const rootElem = document.getElementById("root")
 const root = createRoot(rootElem!);
 root.render(
