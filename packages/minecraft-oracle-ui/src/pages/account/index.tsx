35788bd4a0e9a9eafbe8e66493d8025f1fb62177
misterjame <103995267+misterjame@users.noreply.github.com>
2022-08-15 00:05:25 +0700

    New account page

--- packages/minecraft-oracle-ui/src/pages/account/index.tsx
+++ packages/minecraft-oracle-ui/src/pages/account/index.tsx
@@ -3,12 +3,14 @@ import { AuthLayout, Loader } from 'ui';
 import { useAuth, useClasses, useOauthLogin } from 'hooks';
 import { useNavigate } from 'react-router-dom';
 import axios, { AxiosError } from 'axios';
-import { Alert, AlertDescription, AlertIcon, Box, Button, Stack, Tag, TagCloseButton, TagLabel, TagLeftIcon, TagRightIcon } from '@chakra-ui/react';
-import { DeviceGamepad2, Pencil, Tags, User } from 'tabler-icons-react';
+import { Container, Image, Alert, AlertDescription, AlertIcon, Box, Button, CircularProgress, HStack, Stack, Tag, TagCloseButton, TagLabel, TagLeftIcon, TagRightIcon, VStack, Grid, GridItem } from '@chakra-ui/react';
+import { DeviceGamepad2, Pencil, Power, Tags, User } from 'tabler-icons-react';
 import { useSelector } from 'react-redux';
 import { selectAccessToken, setTokens } from '../../state/slices/authSlice';
 import { useUserProfileQuery } from '../../state/api/bridgeApi';
 import { useDispatch } from 'react-redux';
+import BackgroundImage from '../../assets/images/bridge-background-blur.svg'
+
 const AccountPage = () => {
   const accessToken = useSelector(selectAccessToken)
   const [failureMessage, setFailureMessage] = useState("")
@@ -18,115 +20,93 @@ const AccountPage = () => {
   const dispatch = useDispatch()
 
 
-
-  const handleMinecraftLink = () => {
-    navigate("/account/minecraft/redirect")
-  }
-
-  const handleLogout = () => {
-    setOauthData(null)
-    window.localStorage.removeItem('accessToken');
-    dispatch(setTokens({ accessToken: null, refreshToken: null }));
-
-    // setAuthData({ jwt: undefined })
-  }
-
-  const handleMinecraftUnlink = async () => {
-    navigate("/account/minecraft/unlink")
-  }
-
-  const handleSetGamerTag = () => {
-    navigate("/account/gamertag")
-
-  }
-
-  if (!accessToken) {
-    navigate('/bridge')
-  }
-
-
-
-  let alert
-  if (failureMessage) {
-    alert = { severity: "error", text: failureMessage }
-  }
+  const sectionTitleProps = { fontFamily: "heading", color: "teal.200", fontSize: "16px", lineHeight: "19px" }
+  const sectionDescriptionProps = { fontSize: "14px", lineHeight: "20px", color: "gray.400", paddingTop: "4px" }
   return (
-    <AuthLayout title="ACCOUNT" loading={profileLoading} >
-      <Stack direction="column" alignItems='center' textAlign='center' spacing={0}>
-        <Tag sx={{ maxWidth: 300 }}
-          size={"lg"}
-          key={"lg"}
-          borderRadius='full'
-          variant='solid'
+    <Container
+      bg="#080714"
+      backgroundPosition="top right"
+      backgroundRepeat="no-repeat"
+      backgroundSize='600px 700px'
+      minWidth="100%"
+      margin="0"
+      padding="0"
+      height="100%"
+      position="relative"
+      overflow="visible">
+
+      <Box position="absolute" w="100%" h="100%" bg="#080714">
+        <Image src={BackgroundImage} w="552px" h="622px" position="absolute" top="0" right="0" filter="blur(10px)"></Image>
+      </Box>
+      {false
+        ?
+        <VStack className="moonsamaFullHeight">
+          <HStack h="100%">
+            <CircularProgress isIndeterminate color="teal"></CircularProgress>
+          </HStack>
+        </VStack>
+        :
+        <Grid
+          overflow="hidden"
+          paddingTop="120px"
+          margin="auto"
+          zIndex="2"
+          templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+          w={{ base: "min(calc(100% - 22px), 900px)", md: "min(calc(100% - 80px), 900px)" }}
         >
-          <TagLeftIcon as={User} />
-          <TagLabel>{profile?.email}</TagLabel>
-          <TagRightIcon sx={{ cursor: "pointer" }} as={Pencil} onClick={() => { navigate(`/account/login/email/change`) }} />
-        </Tag>
+          {/** START title + logout */}
+          <GridItem zIndex="2" paddingBottom={{ base: "8px", md: "40px" }}>
+            <VStack alignItems="flex-start" spacing="0">
+              <Box fontFamily="heading" color="teal.200" fontSize="30px" lineHeight="36px">Account Settings</Box>
+              <Box fontSize="16px" lineHeight="24px" color="gray.400" paddingTop="8px">Change your account details</Box>
+            </VStack>
+          </GridItem>
+          <GridItem zIndex="2" paddingBottom="40px">
+            <VStack alignItems={{ base: "flex-end", md: "flex-end" }} spacing="0">
+              <Button variant="moonsamaGhost" rightIcon={<Power color="#3BEFB8" />}>Log Out</Button>
+            </VStack>
+          </GridItem>
+          {/** END title + logout */}
 
-        {/*<Chip color="info" sx={{ maxWidth: 300 }} icon={<User />} label={authData?.userProfile?.email} onDelete={() => { navigate(`/account/login/email/change`) }}
-          deleteIcon={<Pencil />}></Chip>*/}
+          {/** START email */}
+          <GridItem zIndex="2" paddingTop="24px" borderTop="1px solid" borderColor="gray.500">
+            <VStack alignItems="flex-start" spacing="0" >
+              <Box {...sectionTitleProps}>Email Address</Box>
+              <Box {...sectionDescriptionProps}>An accurate email address you will use for login and authentication purposes. Your Moonsama account is based on your email address.</Box>
+            </VStack>
+          </GridItem>
+          <GridItem zIndex="2" paddingTop="24px" borderTop="1px solid" borderColor={{ base: "transparent", md: "gray.500" }}>
+            <Box>aaa</Box>
+          </GridItem>
+          {/** END email */}
 
+          {/** START minecraft */}
+          <GridItem zIndex="2" paddingTop="24px">
+            <VStack alignItems="flex-start" spacing="0">
+              <Box {...sectionTitleProps}>Linked Minecraft Account</Box>
+              <Box {...sectionDescriptionProps}>Linking a Minecraft account that was used with Moonsama prior to the email login system will migrate all your assets to your email address.</Box>
+            </VStack>
+          </GridItem>
+          <GridItem zIndex="2" paddingTop="24px">
+            <Box>aaa</Box>
+          </GridItem>
+          {/** END minecraft */}
 
-        <Stack direction="column" alignItems='center' textAlign='center' spacing={1} marginTop={2}>
-          <Box paddingTop="10px" >
-            <Button style={{ maxWidth: '200px', width: '200px', minWidth: '200px' }} onClick={() => { handleLogout() }} >LOGOUT</Button>
-          </Box>
-        </Stack>
-        <Stack direction="column" alignItems='center' textAlign='center' spacing={1} margin={2} marginTop={5}>
-          <Box paddingTop="30px">LINKED MINECRAFT ACCOUNT</Box>
-          {!profile?.minecraftUuid && <><Alert sx={{ margin: "auto" }} variant='solid' status="warning"><AlertIcon /><AlertDescription textAlign="left" fontFamily="Rubik">Linking a Minecraft account that was used with Moonsama prior to the new login system will migrate over all assets and resources to your Moonsama account. Make sure you do not lose access to your email address.</AlertDescription></Alert><div></div></>}
-          {!!profile?.minecraftUuid && <>
-            <Tag sx={{ maxWidth: 300 }}
-              size={"lg"}
-              key={"lg"}
-              borderRadius='full'
-              variant='solid'
-              colorScheme="green"
-            >
-              <TagLeftIcon as={DeviceGamepad2} />
-              <TagLabel>{profile?.minecraftUserName}</TagLabel>
-            </Tag>
-          </>}
-          <Box>{profile?.minecraftUuid
-            ? <Button style={{ maxWidth: '200px', width: '200px', minWidth: '200px' }} marginTop="6px" onClick={() => { handleMinecraftUnlink() }} >UNLINK MINECRAFT</Button>
-            : <Button style={{ maxWidth: '200px', width: '200px', minWidth: '200px' }} marginTop="4px" onClick={() => { handleMinecraftLink() }} >LINK MINECRAFT</Button>}
-          </Box>
-        </Stack>
-        <Stack direction="column" alignItems='center' textAlign='center' spacing={1} margin={2} marginTop={3}>
-          <Box paddingTop="30px">GAMER TAG</Box>
-          {!!profile?.gamerTag
-            ?
-            <Tag sx={{ maxWidth: 300 }}
-              size={"lg"}
-              key={"lg"}
-              borderRadius='full'
-              variant='solid'
-            >
-              <TagLeftIcon as={Tags} />
-              <TagLabel>{profile?.gamerTag}</TagLabel>
-              <TagRightIcon sx={{ cursor: "pointer" }} as={Pencil} onClick={() => { navigate(`/account/gamertag`) }} />
+          {/** START gamertag */}
+          <GridItem zIndex="2" paddingTop="24px">
+            <VStack alignItems="flex-start" spacing="0">
+              <Box {...sectionTitleProps}>Gamer Tag</Box>
+              <Box {...sectionDescriptionProps}>Choose a gamer tag to be used in the Moonsama Multiverse. Please note this gamer tag does not apply to Minecraft.</Box>
+            </VStack>
+          </GridItem>
+          <GridItem zIndex="2" paddingTop="24px">
+            <Box>aaa</Box>
+          </GridItem>
+          {/** END gamertag */}
 
-            </Tag>
-            :
-            <>
-              <Box></Box>
-              <Button onClick={() => { handleSetGamerTag() }} >SET GAMER TAG</Button>
-
-            </>
-          }
-
-        </Stack>
-        <Stack direction="column" alignItems='center' textAlign='center' spacing={1} margin={2} marginTop={3}>
-          <Box paddingTop="30px" paddingBottom="30px">
-            <Button onClick={() => { navigate("/bridge") }} >GO TO BRIDGE</Button>
-          </Box>
-
-
-        </Stack>
-      </Stack >
-
-    </AuthLayout >
+        </Grid>
+      }
+    </Container>
   );
 };
 
