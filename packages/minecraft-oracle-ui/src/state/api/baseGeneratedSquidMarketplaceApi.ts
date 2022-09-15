85a74ba032228c13959cb7456d3259e5d0482503
misterjame <103995267+misterjame@users.noreply.github.com>
2022-09-15 18:23:03 +0700

    Fix graphql query

--- packages/minecraft-oracle-ui/src/state/api/baseGeneratedSquidMarketplaceApi.ts
+++ packages/minecraft-oracle-ui/src/state/api/baseGeneratedSquidMarketplaceApi.ts
@@ -2,7 +2,7 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
 import { GraphQLClient } from 'graphql-request'
 
-export const client = new GraphQLClient(process.env.REACT_APP_NFT_SQUID_URL as string)
+export const client = new GraphQLClient(process.env.REACT_APP_SQUID_MARKETPLACE_URL as string)
 
 export const api = createApi({
   reducerPath: 'generatedSquidMarketplaceApi',
