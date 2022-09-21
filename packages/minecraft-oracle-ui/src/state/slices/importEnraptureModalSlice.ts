e4ca6bc37ba30223f4d1276b663fb038733f39b5
misterjame <103995267+misterjame@users.noreply.github.com>
2022-09-21 16:17:41 +0700

    More import enrapture work

--- packages/minecraft-oracle-ui/src/state/slices/importEnraptureModalSlice.ts
+++ packages/minecraft-oracle-ui/src/state/slices/importEnraptureModalSlice.ts
@@ -1,24 +1,30 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { AppState } from ".."
+import { OnChainTokenWithRecognizedTokenData } from "../../utils/graphqlReformatter"
 
 export interface ImportEnraptureModalSlice {
     modalOpen: boolean,
+    importEnraptureTokens: OnChainTokenWithRecognizedTokenData[]
 }
 
 const importEnraptureModalSlice = createSlice({
     name: "importEnraptureModalSlice",
-    initialState: { modalOpen: false } as ImportEnraptureModalSlice,
+    initialState: { modalOpen: false, importEnraptureTokens: [] } as ImportEnraptureModalSlice,
     reducers: {
         closeImportEnraptureModal: (state) => {
             state.modalOpen = false
         },
         openImportEnraptureModal: (state) => {
             state.modalOpen = true
+        },
+        setImportEnraptureTokens: (state, importEnraptureTokens: PayloadAction<OnChainTokenWithRecognizedTokenData[]>) => {
+            state.importEnraptureTokens = importEnraptureTokens.payload
         }
     }
 })
 
-export const { closeImportEnraptureModal, openImportEnraptureModal } = importEnraptureModalSlice.actions
+export const { closeImportEnraptureModal, openImportEnraptureModal, setImportEnraptureTokens } = importEnraptureModalSlice.actions
 export default importEnraptureModalSlice.reducer
 
 export const selectImportEnraptureModalOpen = (state: AppState) => state?.importEnraptureModal?.modalOpen
+export const selectImportEnraptureModalTokens = (state: AppState) => state?.importEnraptureModal?.importEnraptureTokens
