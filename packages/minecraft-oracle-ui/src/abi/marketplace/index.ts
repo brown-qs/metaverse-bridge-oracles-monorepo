eaa84794f8b2beaaaedb7b1d8b8f89723dfb87e2
misterjame <103995267+misterjame@users.noreply.github.com>
2022-10-06 22:16:31 +0700

    Signing crapping out

--- packages/minecraft-oracle-ui/src/abi/marketplace/index.ts
+++ packages/minecraft-oracle-ui/src/abi/marketplace/index.ts
@@ -2,5 +2,6 @@ import MARKETPLACE_V1_ABI from './MarketplaceV1.json';
 import WAREHOUSE_ABI from './Warehouse.json';
 import RECOGNIZED_COLLECTIONS_ABI from './RecognizedCollections.json';
 import METAVERSE_V1_ABI from './Metaverse.json';
+import METAVERSE_V2_ABI from './MetaverseV2.json';
 
-export { MARKETPLACE_V1_ABI, WAREHOUSE_ABI, RECOGNIZED_COLLECTIONS_ABI, METAVERSE_V1_ABI };
+export { MARKETPLACE_V1_ABI, METAVERSE_V2_ABI, WAREHOUSE_ABI, RECOGNIZED_COLLECTIONS_ABI, METAVERSE_V1_ABI };
