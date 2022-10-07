d5879f34e56b9cce86e5aff79039dac7d00f7a4e
misterjame <103995267+misterjame@users.noreply.github.com>
2022-10-07 21:21:43 +0700

    Add mv version to in game stuff

--- packages/minecraft-oracle-api/src/profileapi/profileapi.service.ts
+++ packages/minecraft-oracle-api/src/profileapi/profileapi.service.ts
@@ -3,7 +3,7 @@ import { ProfileDto } from './dtos/profile.dto';
 import { WINSTON_MODULE_NEST_PROVIDER, WinstonLogger } from 'nest-winston';
 import { AssetService } from '../asset/asset.service';
 import { UserEntity } from '../user/user/user.entity';
-import { RecognizedAssetType, PlayEligibilityReason } from '../config/constants';
+import { RecognizedAssetType, PlayEligibilityReason, MultiverseVersion } from '../config/constants';
 import { ProviderToken } from '../provider/token';
 import { AssetDto, TextureDto, ThingsDto } from './dtos/things.dto';
 import { InventoryService } from '../playerinventory/inventory.service';
@@ -60,125 +60,6 @@ export class ProfileApiService {
         return textures
     }
 
-    async getPlayerItems(user: UserEntity): Promise<ThingsDto> {
-        const snapshots = await this.inventoryService.findMany({ relations: ['material', 'owner'], where: { owner: { uuid: user.uuid } } })
-
-        const resources: AssetDto[] = snapshots.map(snapshot => {
-            return {
-                amount: snapshot.amount,
-                assetAddress: snapshot.material.assetAddress,
-                assetType: snapshot.material.assetType,
-                assetId: snapshot.material.assetId,
-                name: snapshot.material.name,
-                exportable: false,
-                summonable: true,
-                recognizedAssetType: '',
-                enraptured: false,
-                chainId: 1285, // resources are multi chain
-                exportAddress: undefined,
-            }
-        })
-
-        const userAssets = await this.assetService.findMany({ where: { owner: user.uuid, pendingIn: false }, relations: ['collectionFragment', 'collectionFragment.collection'], loadEagerRelations: true })
-        const userSkins = await this.skinService.findMany({ where: { owner: user.uuid }, relations: ['texture'] })
-
-        const importableAssets = await this.getRecognizedAssets(BridgeAssetType.IMPORTED)
-        const enrapturableAssets = await this.getRecognizedAssets(BridgeAssetType.ENRAPTURED)
-
-
-        const assets: AssetDto[] = []
-
-        for (let i = 0; i < userAssets.length; i++) {
-            const asset = userAssets[i]
-
-            const assetAddress = asset.collectionFragment.collection.assetAddress.toLowerCase()
-            const recongizedEnraptureAsset = findRecognizedAsset(enrapturableAssets, { assetAddress, assetId: asset.assetId })
-
-            if (!!recongizedEnraptureAsset && recongizedEnraptureAsset.recognizedAssetType.valueOf() !== RecognizedAssetType.RESOURCE.valueOf()) {
-                assets.push({
-                    amount: asset.amount,
-                    assetAddress,
-                    assetType: asset.collectionFragment.collection.assetType,
-                    assetId: asset.assetId,
-                    name: recongizedEnraptureAsset.name,
-                    exportable: !asset.enraptured,
-                    hash: asset.hash,
-                    summonable: false,
-                    recognizedAssetType: recongizedEnraptureAsset.recognizedAssetType.valueOf(),
-                    enraptured: asset.enraptured,
-                    chainId: asset.collectionFragment.collection.chainId,
-                    exportAddress: asset.assetOwner?.toLowerCase(),
-                })
-                continue
-            }
-
-            const recongizedImportAsset = findRecognizedAsset(importableAssets, { assetAddress, assetId: asset.assetId })
-
-            if (!!recongizedImportAsset) {
-                assets.push({
-                    amount: asset.amount,
-                    assetAddress: asset.collectionFragment.collection.assetAddress.toLowerCase(),
-                    assetType: asset.collectionFragment.collection.assetType,
-                    assetId: asset.assetId,
-                    name: recongizedImportAsset.name,
-                    exportable: !asset.enraptured,
-                    hash: asset.hash,
-                    summonable: false,
-                    recognizedAssetType: recongizedImportAsset.recognizedAssetType.valueOf(),
-                    enraptured: asset.enraptured,
-                    chainId: asset.collectionFragment.collection.chainId,
-                    exportAddress: asset.assetOwner?.toLowerCase(),
-                })
-                continue
-            }
-        }
-
-        const textures: TextureDto[] = userSkins.map(skin => {
-            return {
-                id: skin.id,
-                assetAddress: skin.texture.assetAddress,
-                assetId: skin.texture.assetId,
-                assetType: skin.texture.assetType,
-                equipped: skin.equipped,
-                selectable: true,
-                textureData: skin.texture.textureData,
-                textureSignature: skin.texture.textureSignature,
-                name: skin.texture.name
-            }
-        })
-
-
-        // TODO fixme
-        const bait = await this.resourceInventoryService.findOne({ owner: user }, { relations: ['owner', 'offset'] })
-        if (!!bait) {
-            const baitAsset = userAssets.find(x => x.assetId === bait.assetId && x.collectionFragment.recognizedAssetType.valueOf() === RecognizedAssetType.RESOURCE.valueOf())
-
-            if (!!baitAsset) {
-                assets.push(
-                    {
-                        amount: formatEther(BigNumber.from(bait.amount).sub(bait.offset?.amount ?? '0')),
-                        assetAddress: baitAsset.collectionFragment.collection.assetAddress.toLowerCase(),
-                        assetType: baitAsset.collectionFragment.collection.assetType,
-                        assetId: baitAsset.assetId,
-                        name: baitAsset.collectionFragment.name,
-                        exportable: !baitAsset.enraptured,
-                        hash: baitAsset.hash,
-                        summonable: false,
-                        recognizedAssetType: baitAsset.recognizedAssetType.valueOf(),
-                        enraptured: baitAsset.enraptured,
-                        chainId: baitAsset.collectionFragment.collection.chainId,
-                        exportAddress: baitAsset.assetOwner?.toLowerCase(),
-                    }
-                )
-            }
-        }
-
-        return {
-            resources,
-            assets,
-            textures
-        }
-    }
 
     async getInGameItems(user: UserEntity): Promise<AssetDto[]> {
         const importableAssets = await this.getRecognizedAssets(BridgeAssetType.IMPORTED)
@@ -203,6 +84,7 @@ export class ProfileApiService {
                     enraptured: asset.enraptured,
                     chainId: asset.collectionFragment.collection.chainId,
                     exportAddress: asset.assetOwner?.toLowerCase(),
+                    multiverseVersion: asset.collectionFragment.collection.multiverseVersion
                 })
                 continue
             }
@@ -223,6 +105,7 @@ export class ProfileApiService {
                     enraptured: asset.enraptured,
                     chainId: asset.collectionFragment.collection.chainId,
                     exportAddress: asset.assetOwner?.toLowerCase(),
+                    multiverseVersion: asset.collectionFragment.collection.multiverseVersion
                 })
                 continue
             }
@@ -246,6 +129,8 @@ export class ProfileApiService {
                 enraptured: false,
                 chainId: 1285, // resources are multi chain
                 exportAddress: undefined,
+                multiverseVersion: MultiverseVersion.V1
+
             }
         })
         return resources
