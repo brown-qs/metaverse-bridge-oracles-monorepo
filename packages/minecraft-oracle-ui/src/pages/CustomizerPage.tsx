332844424a5f47e085c8df1096d7b9dd8c027cee
misterjame <103995267+misterjame@users.noreply.github.com>
2022-10-18 21:41:58 +0700

    Add new pages and routes

---
+++ packages/minecraft-oracle-ui/src/pages/CustomizerPage.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import { useClasses } from 'hooks';
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, CloseButton, Heading, HStack, Stack, Tag, TagCloseButton, TagLabel, TagLeftIcon, TagRightIcon, useToast, VStack } from '@chakra-ui/react';
+import { DeviceGamepad2, Pencil, Tags, User } from 'tabler-icons-react';
+
+const CustomizerPage = () => {
+  return <></>
+}
+export default CustomizerPage;
