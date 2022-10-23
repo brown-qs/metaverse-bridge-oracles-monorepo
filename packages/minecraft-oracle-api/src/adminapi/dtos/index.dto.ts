b1c156a635e61958b3b954710692b5fe6e48fcb7
misterjame <103995267+misterjame@users.noreply.github.com>
2022-10-23 16:09:51 +0700

    Asset recover finished

--- packages/minecraft-oracle-api/src/adminapi/dtos/index.dto.ts
+++ packages/minecraft-oracle-api/src/adminapi/dtos/index.dto.ts
@@ -1,9 +1,37 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsNotEmpty, IsString } from "class-validator"
+import { IsDefined, IsEthereumAddress, IsInt, IsNotEmpty, IsNotEmptyObject, IsNumberString, IsObject, IsString, ValidateNested } from "class-validator"
+import { Type } from 'class-transformer';
+import { InRequestDto } from "../../oracleapi/dtos/index.dto"
 
 export class UpdateMetadataDto {
     @ApiProperty({ description: 'Hash of staked item', required: true })
     @IsNotEmpty()
     @IsString()
     hash: string
+}
+
+
+export class RecoverAssetDto {
+    @ApiProperty({ description: 'Hash of staked item', required: true })
+    @IsNotEmpty()
+    @IsString()
+    hash: string
+
+    @ApiProperty({ description: 'Salt of staked item', required: true })
+    @IsNotEmpty()
+    @IsString()
+    salt: string
+
+    @ApiProperty({ description: 'Uuid of staker', required: true })
+    @IsNotEmpty()
+    @IsString()
+    uuid: string
+
+    @ApiProperty({ description: 'InRequestDto of staked item', required: true })
+    @IsDefined()
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => InRequestDto)
+    inData: InRequestDto
 }
