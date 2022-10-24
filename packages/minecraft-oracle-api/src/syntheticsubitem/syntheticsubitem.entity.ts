1f142bf7bfdc0af623be32f334a237e504ead69b
misterjame <103995267+misterjame@users.noreply.github.com>
2022-10-25 01:18:28 +0700

    Images in repo

---
+++ packages/minecraft-oracle-api/src/syntheticsubitem/syntheticsubitem.entity.ts
@@ -0,0 +1,20 @@
+import { CompositeAssetEntity } from '../compositeasset/compositeasset.entity';
+import { Column, Entity, ManyToMany, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { SyntheticPartEntity } from '../syntheticpart/syntheticpart.entity';
+import { SyntheticItemEntity } from '../syntheticitem/syntheticitem.entity';
+
+@Entity()
+export class SyntheticSubItemEntity {
+
+    @PrimaryColumn()
+    id: string
+
+    @Column({ nullable: true })
+    description: string
+
+    @Column({ nullable: false, default: 0 })
+    zIndex: number
+
+    @ManyToOne(() => SyntheticItemEntity, (en) => en.subItems, { nullable: false })
+    syntheticItem: SyntheticItemEntity;
+}
