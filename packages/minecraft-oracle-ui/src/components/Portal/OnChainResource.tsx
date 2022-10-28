7fc7a0762611cdb9fd0c1f5084c7b36e818726df
misterjame <103995267+misterjame@users.noreply.github.com>
2022-10-29 00:20:15 +0700

    Major overhaul

--- packages/minecraft-oracle-ui/src/components/Portal/OnChainResource.tsx
+++ packages/minecraft-oracle-ui/src/components/Portal/OnChainResource.tsx
@@ -1,16 +1,36 @@
 
+import React from "react";
 import { ReactNode } from "react";
-import { StandardizedOnChainTokenWithRecognizedTokenData } from "../../utils/graphqlReformatter";
-import { PortalTabListBalanceProps, PortalTabListClickableProps, PortalTabListItem } from "./PortalTabListItem";
+import { formatOnChainTokenName, formatOnChainTokenSuffix, StandardizedOnChainTokenWithRecognizedTokenData } from "../../utils/graphqlReformatter";
+import { PortalTabListBalanceProps, PortalTabListClickableProps, PortalTabListItem, PortalTabListItemProps } from "./PortalTabListItem";
+import { BigNumber } from "@ethersproject/bignumber";
 
 export type OnChainResourceProps = {
     token: StandardizedOnChainTokenWithRecognizedTokenData,
     children?: ReactNode
 } & PortalTabListClickableProps
 
-export const OnChainResource: React.FC<OnChainResourceProps> = ({ ...inProps }) => {
-    const props = {
-        ...inProps,
+export const OnChainResource: React.FC<OnChainResourceProps> = ({ token, ...inProps }) => {
+    const lineOne: string | undefined = React.useMemo(() => formatOnChainTokenName(token), [token])
+    const lineOneSuffix: string | undefined = React.useMemo(() => formatOnChainTokenSuffix(token), [token])
+
+    const props: PortalTabListItemProps = {
+        mediaUrl: token?.metadata?.image ?? undefined,
+        isLoading: false,
+        lineOne,
+        lineOneSuffix,
+        mediaRedOutline: false,
+        lineTwo: undefined,
+        highlightable: false,
+
+        onCheckboxChange: undefined,
+        isChecked: undefined,
+        checkboxValue: undefined,
+        isCheckboxDisabled: undefined,
+
+        balanceEther: BigNumber.from(token.balance).div(BigNumber.from(10).pow(token.decimals)),
+
+        ...inProps
     }
     return (
         <PortalTabListItem
