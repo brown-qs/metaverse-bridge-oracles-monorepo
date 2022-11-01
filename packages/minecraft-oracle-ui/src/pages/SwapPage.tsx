b7cb8b613988dfab4a25d76be850a9e52f5c59d6
misterjame <103995267+misterjame@users.noreply.github.com>
2022-11-01 21:25:22 +0700

    fix margin

--- packages/minecraft-oracle-ui/src/pages/SwapPage.tsx
+++ packages/minecraft-oracle-ui/src/pages/SwapPage.tsx
@@ -7,25 +7,19 @@ import BackgroundImage from '../assets/images/bridge-background-blur.svg'
 const SwapPage = () => {
   return (
     <>
-      <Box w="100%" h="100%"
-        bg="radial-gradient(circle at 26.53% 49.03%, #391622, transparent 40%),radial-gradient(circle at 99.49% 0.76%, #52274B, transparent 31%),radial-gradient(circle at 97.03% 42.36%, #391622, transparent 34%),radial-gradient(circle at 71.08% 5.96%, #391622, transparent 28%),radial-gradient(circle at 50% 50%, #080714, #080714 100%)"
-        position="absolute"
-        top="0"
-        left="0"
-        right="-20px"
-        className="moonsamaFullHeight"
-      ></Box>
+
       <VStack
         zIndex="1"
         direction={'column'}
         justifyContent='center'
         alignItems='center'
         spacing="0"
-        position="absolute"
-        top="0"
-        left="0"
-        right="0"
+        position="relative"
+        left="calc(-1 * var(--moonsama-leftright-padding))"
         className="moonsamaFullHeight"
+        w="100vw"
+        overflow="hidden"
+        bg="radial-gradient(circle at 26.53% 49.03%, #391622, transparent 40%),radial-gradient(circle at 99.49% 0.76%, #52274B, transparent 31%),radial-gradient(circle at 97.03% 42.36%, #391622, transparent 34%),radial-gradient(circle at 71.08% 5.96%, #391622, transparent 28%),radial-gradient(circle at 50% 50%, #080714, #080714 100%)"
       >
 
 
