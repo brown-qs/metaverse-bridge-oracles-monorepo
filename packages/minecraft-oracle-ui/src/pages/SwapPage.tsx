65163b25028e9254f3a0076c4066b662f908d7a5
misterjame <103995267+misterjame@users.noreply.github.com>
2022-11-01 21:08:58 +0700

    Swap page work

--- packages/minecraft-oracle-ui/src/pages/SwapPage.tsx
+++ packages/minecraft-oracle-ui/src/pages/SwapPage.tsx
@@ -6,74 +6,88 @@ import BackgroundImage from '../assets/images/bridge-background-blur.svg'
 
 const SwapPage = () => {
   return (
-    <VStack
-      zIndex="1"
-      direction={'column'}
-      justifyContent='center'
-      alignItems='center'
-      spacing="0"
-      position="absolute"
-      top="0"
-      left="0"
-      right="0"
-      className="moonsamaFullHeight"
-    >
+    <>
+      <Box w="100%" h="100%"
+        bg="radial-gradient(circle at 26.53% 49.03%, #391622, transparent 40%),radial-gradient(circle at 20.31% 20.56%, rgba(255,255,255,0.5), transparent 2%),radial-gradient(circle at 84.33% 72.59%, rgba(255,255,255,0.5), transparent 2%),radial-gradient(circle at 99.49% 0.76%, #52274B, transparent 31%),radial-gradient(circle at 97.03% 42.36%, #391622, transparent 34%),radial-gradient(circle at 71.08% 5.96%, #391622, transparent 28%),radial-gradient(circle at 50% 50%, #080714, #080714 100%)"
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        className="moonsamaFullHeight"
+        filter="blur(30px)"
 
+      ></Box>
       <VStack
+        zIndex="1"
+        direction={'column'}
+        justifyContent='center'
+        alignItems='center'
         spacing="0"
-        width="min(calc(100% - 70px), 896px)"
-
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        className="moonsamaFullHeight"
       >
-        <HStack spacing="0">
-          <Box>
-            <Box w={{ base: "100px", sm: "140px" }} h={{ base: "100px", sm: "140px" }} bg="white"></Box>
-            <Box textAlign="center" fontSize="24px" lineHeight="32px">$POOP</Box>
-          </Box>
-          <Box w="30px"></Box>
-          <Box>
-            <Box w={{ base: "100px", sm: "140px" }} h={{ base: "100px", sm: "140px" }} bg="white"></Box>
-            <Box textAlign="center" fontSize="24px" lineHeight="32px">$SAMA</Box>
 
-          </Box>
-        </HStack>
-        <Box h="40px"></Box>
+
+
         <VStack
-          bg="whiteAlpha.100"
-          borderRadius="8px"
-          padding="32px"
           spacing="0"
-          w="100%">
-          <Box
+          width="min(calc(100% - 70px), 896px)"
+
+        >
+          <HStack spacing="0">
+            <Box>
+              <Box w={{ base: "100px", sm: "140px" }} h={{ base: "100px", sm: "140px" }} bg="white"></Box>
+              <Box textAlign="center" fontSize="24px" lineHeight="32px">$POOP</Box>
+            </Box>
+            <Box w="30px"></Box>
+            <Box>
+              <Box w={{ base: "100px", sm: "140px" }} h={{ base: "100px", sm: "140px" }} bg="white"></Box>
+              <Box textAlign="center" fontSize="24px" lineHeight="32px">$SAMA</Box>
+
+            </Box>
+          </HStack>
+          <Box h="40px"></Box>
+          <VStack
+            bg="whiteAlpha.100"
+            borderRadius="8px"
+            padding="32px"
+            spacing="0"
+            w="100%">
+            <Box
 
 
-            color="teal.200"
-            fontFamily="Orbitron"
-            fontWeight="500"
-            w="100%"
-            textAlign="center"
-            fontSize="30px"
-            lineHeight="36px"
-          >
-            1234
-          </Box>
-          <Box h="12px"></Box>
-          <Box
-            w="100%"
-            textAlign="center"
-            fontSize="14px"
-            lineHeight="20px"
-            fontFamily="Rubik"
-            fontWeight="400"
-          >
-            CLICK THE BUTTON TO SWAP THIS AMOUNT OF <Box as="span" fontWeight="700">$POOP</Box> FOR <Box as="span" fontWeight="700">$SAMA</Box>
-          </Box>
-          <Box h="12px"></Box>
-          <Box w="100%">
-            <Button w="100%">SWAP $POOP FOR $SAMA</Button>
-          </Box>
+              color="teal.200"
+              fontFamily="Orbitron"
+              fontWeight="500"
+              w="100%"
+              textAlign="center"
+              fontSize="30px"
+              lineHeight="36px"
+            >
+              1234
+            </Box>
+            <Box h="12px"></Box>
+            <Box
+              w="100%"
+              textAlign="center"
+              fontSize="14px"
+              lineHeight="20px"
+              fontFamily="Rubik"
+              fontWeight="400"
+            >
+              CLICK THE BUTTON TO SWAP THIS AMOUNT OF <Box as="span" fontWeight="700">$POOP</Box> FOR <Box as="span" fontWeight="700">$SAMA</Box>
+            </Box>
+            <Box h="12px"></Box>
+            <Box w="100%">
+              <Button w="100%">SWAP $POOP FOR $SAMA</Button>
+            </Box>
+          </VStack>
         </VStack>
       </VStack>
-    </VStack>
+    </>
   )
 }
 
