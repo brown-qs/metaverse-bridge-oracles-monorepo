78f39203f9a7de1113652fb5371bc2fa10d2f11e
misterjame <103995267+misterjame@users.noreply.github.com>
2022-11-06 20:11:33 +0700

    Exosama hack again

---
+++ packages/minecraft-oracle-api/src/views/asset-totals.view.ts
@@ -0,0 +1,34 @@
+import { Post } from "@nestjs/common";
+import { Category } from "fp-ts/lib/Reader";
+import { ViewEntity, Connection, ViewColumn, Column } from "typeorm";
+import { AssetEntity } from "../asset/asset.entity";
+import { CollectionFragmentEntity } from "../collectionfragment/collectionfragment.entity";
+import { UserEntity } from "../user/user/user.entity";
+
+@ViewEntity({
+    expression: (connection: Connection) => {
+
+        //add limit so view is not editable
+        //https://stackoverflow.com/questions/58026241/how-to-create-a-read-only-view-in-postgresql-similar-to-oracle
+        return connection.createQueryBuilder()
+            .select(`collection_fragment.name`, "name")
+            .addSelect(`collection_fragment.recognizedAssetType`, "recognizedAssetType")
+            .from(CollectionFragmentEntity, "collection_fragment")
+            .orderBy("collection_fragment.name", "ASC")
+            .limit(1000 * 1000 * 1000)
+
+    }
+})
+export class ZAssetTotalsView {
+    @ViewColumn()
+    uuid: string
+
+    @ViewColumn()
+    email: string
+
+    @ViewColumn()
+    minecraftUserName: string
+
+    @ViewColumn()
+    collectionName: string
+}
