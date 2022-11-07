ed3ae4f1a04428954c4d245b69575b84ae98a547
brown-qs <neoprince1992@gmail.com>
2022-11-07 20:28:27 +0700

    Nav margins

--- packages/minecraft-oracle-ui/src/pages/CustomizerLibrary.tsx
+++ packages/minecraft-oracle-ui/src/pages/CustomizerLibrary.tsx
@@ -11,34 +11,43 @@ import useAddNetworkToMetamaskCb from '../hooks/useAddNetworkToMetamask/useAddNe
 
 const CustomizerLibrary = () => {
   const { account, chainId, library } = useActiveWeb3React()
-  return (
-    <Box bg="gray.800" className="moonsamaFullHeight" position="relative" left="calc(-1 * var(--moonsama-leftright-padding))" w="100vw">
-      <Stack spacing="0" w="100%" h="100%" direction={{ base: "column", md: "row" }}>
-        {/** START Library Nav, on chain/wallet, etc */}
-        <Stack spacing="0" w="225px" bg="gray.800" paddingLeft="var(--moonsama-leftright-padding)" direction={{ base: "row", md: "column" }}>
-          <VStack spacing="0" fontWeight="500" fontFamily="Rubik" alignItems="flex-start" paddingTop="16px">
-            <Box color="gray.400" fontSize="12px" lineHeight="16px">
-              MY NFTS
-            </Box>
-            <Button leftIcon={<Stack3 color="var(--chakra-colors-teal-200)" />} w="120px" variant="moonsamaGhost">Staked</Button>
-            <Button leftIcon={<Wallet color="var(--chakra-colors-teal-200)" />} w="120px" variant="moonsamaGhost">Wallet</Button>
-          </VStack>
-          <VStack spacing="0" fontWeight="500" fontFamily="Rubik" alignItems="flex-start" paddingTop="16px">
-            <Box color="gray.400" fontSize="12px" lineHeight="16px">
-              MULTIVERSE NFTS
-            </Box>
-            <Button w="120px" variant="moonsamaGhost">Exosama&nbsp;</Button>
-            <Button w="120px" variant="moonsamaGhost">Moonsama</Button>
-          </VStack>
-        </Stack>
-        {/** End Library Nav, on chain/wallet, etc */}
 
-        <Box flex="1" bg="gray.900" paddingRight="var(--moonsama-leftright-padding)">
-          aa
-        </Box>
-      </Stack>
-    </Box >
-  )
+
+  const libraryPage = () => {
+    return (
+      <Box bg="gray.800" className="moonsamaFullHeight" position="relative" left="calc(-1 * var(--moonsama-leftright-padding))" w="100vw">
+        <Stack spacing="0" w="100%" h="100%" direction={{ base: "column", md: "row" }}>
+          {/** START Library Nav, on chain/wallet, etc */}
+          <Stack fontWeight="400" fontFamily="Orbitron" spacing="0" w={{ base: "100%", md: "225px" }} bg="gray.800" paddingBottom="16px" paddingLeft="var(--moonsama-leftright-padding)" direction={{ base: "row", md: "column" }} justifyContent={{ base: "space-around", md: "flex-start" }}>
+            <VStack spacing="0" alignItems="flex-start" paddingTop="16px">
+              <Box color="gray.400" fontSize="12px" lineHeight="16px">
+                MY NFTS
+              </Box>
+              <Box h="8px"></Box>
+              <Button leftIcon={<Stack3 color="var(--chakra-colors-teal-200)" />} w="120px" variant="moonsamaGhost">Staked</Button>
+              <Box h="8px"></Box>
+              <Button leftIcon={<Wallet color="var(--chakra-colors-teal-200)" />} w="120px" variant="moonsamaGhost">Wallet</Button>
+            </VStack>
+            <VStack spacing="0" alignItems="flex-start" paddingTop="16px">
+              <Box color="gray.400" fontSize="12px" lineHeight="16px">
+                MULTIVERSE NFTS
+              </Box>
+              <Box h="8px"></Box>
+              <Button w="120px" variant="moonsamaGhost">Exosama&nbsp;</Button>
+              <Box h="8px"></Box>
+              <Button w="120px" variant="moonsamaGhost">Moonsama</Button>
+            </VStack>
+          </Stack>
+          {/** End Library Nav, on chain/wallet, etc */}
+
+          <Box flex="1" bg="gray.900" padding="16px" paddingRight="var(--moonsama-leftright-padding)">
+            aa
+          </Box>
+        </Stack>
+      </Box >
+    )
+  }
+  return libraryPage()
 }
 
 export default CustomizerLibrary
