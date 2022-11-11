fd7d0a0fa02d32ffc29e17f22d08c0079bc2ed51
misterjame <103995267+misterjame@users.noreply.github.com>
2022-11-11 19:46:43 +0700

    Sama logo and other fixes

--- packages/minecraft-oracle-api/src/oracleapi/dtos/summon.dto.ts
+++ packages/minecraft-oracle-api/src/oracleapi/dtos/summon.dto.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsNumber, IsString } from "class-validator"
+import { IsEthereumAddress, IsNotEmpty, IsNumber, IsString } from "class-validator"
 
 export class SummonDto {
 
-    @ApiProperty({ description: 'Recipient of the asset'})
+    @ApiProperty({ description: 'Recipient of the asset' })
+    @IsNotEmpty()
+    @IsEthereumAddress()
     @IsString()
     recipient: string;
 
-    @ApiProperty({ description: 'Recipient of the asset'})
+    @ApiProperty({ description: 'Recipient of the asset' })
     @IsNumber()
     chainId: number;
 }
