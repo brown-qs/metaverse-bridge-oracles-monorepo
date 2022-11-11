6fcf90708a15b351283c21e525df3807d2904ef8
misterjame <103995267+misterjame@users.noreply.github.com>
2022-11-11 21:10:48 +0700

    Choose a gamepass

--- packages/minecraft-oracle-ui/src/state/slices/summonModalSlice.ts
+++ packages/minecraft-oracle-ui/src/state/slices/summonModalSlice.ts
@@ -1,24 +1,30 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { AppState } from ".."
+import { SummonAddress } from "../../pages/ProfilePage"
 
 export interface SummonModalSlice {
     modalOpen: boolean,
+    summonAddresses: SummonAddress[]
 }
 
 const summonModalSlice = createSlice({
     name: "summonModalSlice",
-    initialState: { modalOpen: false } as SummonModalSlice,
+    initialState: { modalOpen: false, summonAddresses: [] } as SummonModalSlice,
     reducers: {
         closeSummonModal: (state) => {
             state.modalOpen = false
         },
         openSummonModal: (state) => {
             state.modalOpen = true
+        },
+        setSummonModalSummonAddresses: (state, action: PayloadAction<SummonAddress[]>) => {
+            state.summonAddresses = action.payload
         }
     }
 })
 
-export const { closeSummonModal, openSummonModal } = summonModalSlice.actions
+export const { closeSummonModal, openSummonModal, setSummonModalSummonAddresses } = summonModalSlice.actions
 export default summonModalSlice.reducer
 
 export const selectSummonModalOpen = (state: AppState) => state?.summonModal?.modalOpen
+export const selectSummonModalSummonAddresses = (state: AppState) => state?.summonModal?.summonAddresses
