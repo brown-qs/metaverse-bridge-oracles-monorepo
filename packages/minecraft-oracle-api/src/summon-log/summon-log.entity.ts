46414830b32bb16e9d484eea2a97d89ca71d1b28
misterjame <103995267+misterjame@users.noreply.github.com>
2022-11-12 19:57:23 +0700

    Summon logging

--- packages/minecraft-oracle-api/src/summon-log/summon-log.entity.ts
+++ packages/minecraft-oracle-api/src/summon-log/summon-log.entity.ts
@@ -3,7 +3,28 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 @Entity()
 export class SummonLogEntity {
     @PrimaryGeneratedColumn()
-    id: number;
+    id?: number;
+
+    @Column({ nullable: false })
+    uuid: string
+
+    @Column({ nullable: false })
+    chainId: number
+
+    @Column({ nullable: false })
+    assetAddress: string
+
+    @Column('text', { array: true, default: [] })
+    assetIds: string[];
+
+    @Column('text', { array: true, default: [] })
+    assetAmounts: string[];
+
+    @Column({ nullable: false })
+    recipient: string
+
+    @Column({ nullable: false })
+    transactionHash: string
 
     @Column({ type: "timestamptz", nullable: false })
     createdAt: Date
