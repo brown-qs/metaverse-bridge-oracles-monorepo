8ed83a49a59c926d877a5fc5a15bc4c9bcd1e586
misterjame <103995267+misterjame@users.noreply.github.com>
2022-11-23 23:23:12 +0700

    Customizer canvas

---
+++ packages/minecraft-oracle-ui/src/state/slices/customizerSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { AppState } from ".."
+import { ChainId } from "../../constants"
+import { StandardizedOnChainTokenWithRecognizedTokenData } from "../../utils/graphqlReformatter"
+import { CompositeConfigItemDto } from "../api/types"
+
+export interface CustomizerCustomization {
+    parentChainId: ChainId,
+    parentAssetAddress: string,
+    parentAssetId: number,
+    items: CompositeConfigItemDto[]
+}
+export interface CustomizerSlice {
+    customizations: CustomizerCustomization[]
+}
+
+const customizerSlice = createSlice({
+    name: "customizerSlice",
+    initialState: { customizations: [] } as CustomizerSlice,
+    reducers: {
+
+        setMigrateModalTokens: (state, action: PayloadAction<StandardizedOnChainTokenWithRecognizedTokenData[]>) => {
+            //   state.tokens = action.payload
+        }
+    }
+})
+
+export const { setMigrateModalTokens } = customizerSlice.actions
+export default customizerSlice.reducer
+
+export const selectMigrateModalOpen = (state: AppState) => state?.migrateModal?.modalOpen
+export const selectMigrateModalTokens = (state: AppState) => state?.migrateModal?.tokens
