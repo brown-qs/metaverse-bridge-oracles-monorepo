bc68e6510de37dff3f75ba85c363e42862b2d473
misterjame <103995267+misterjame@users.noreply.github.com>
2022-11-28 02:10:35 +0700

    Exo customizer

--- packages/minecraft-oracle-ui/src/components/Customizer/TraitCustomizerCard.tsx
+++ packages/minecraft-oracle-ui/src/components/Customizer/TraitCustomizerCard.tsx
@@ -1,22 +1,49 @@
 import { Box, VStack, Image, Fade, HStack, Button, Tag, Link } from "@chakra-ui/react"
 import { keyframes } from "@emotion/css"
 import React from "react"
+import { useSelector, useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { PhotoOff } from "tabler-icons-react"
+import { ChainId } from "../../constants"
+import state from "../../state"
+import store, { AppState } from "../../state"
+import { selectCustomizerCustomizations, CustomizerCustomization, selectCustomizationAsset, SelectAsset, removeCustomizerAssets, addCustomizerAssets } from "../../state/slices/customizerSlice"
 import CustomizerCard, { CustomizerCardProps } from "./CustomizerCard"
 
 type TraitCustomizerCardProps = {
     owned: boolean,
-    equipped: boolean,
     default: boolean,
     chainId: number,
     assetAddress: string,
     assetId: number,
     synthetic: boolean,
-    traitValue: string
+    traitValue: string,
+    parentChainId: ChainId,
+    parentAssetAddress: string,
+    parentAssetId: number
 } & Omit<CustomizerCardProps, "selected">
 
-const TraitCustomizerCard = ({ imageUrl, width, chainId, assetAddress, assetId, height, owned, equipped, traitValue }: TraitCustomizerCardProps) => {
+const TraitCustomizerCard = ({ imageUrl, width, chainId, assetAddress, assetId, height, owned, traitValue, parentChainId, parentAssetAddress, parentAssetId }: TraitCustomizerCardProps) => {
+    const customizations = useSelector(selectCustomizerCustomizations)
+    const dispatch = useDispatch()
+
+    const selectedAsset: SelectAsset = React.useMemo(() => {
+        return {
+            chainId,
+            assetAddress,
+            assetId,
+            parentChainId,
+            parentAssetAddress,
+            parentAssetId
+        }
+    }, [parentChainId, parentAssetAddress, parentAssetId, chainId, assetAddress, assetId])
+    const cardData = useSelector((state: AppState) => selectCustomizationAsset(state, selectedAsset))
+
+    /*
+    const currentCustomization: CustomizerCustomization | undefined = React.useMemo(() => {
+        const currCustomization = customizations.find(c => c.parentChainId === parentChainId && c.parentAssetAddress === parentAssetAddress && c.parentAssetId === parentAssetId)
+        return currCustomization
+    }, [customizations])*/
 
     const navigate = useNavigate()
     const marketplaceUrl: string = React.useMemo(() => {
@@ -36,9 +63,8 @@ const TraitCustomizerCard = ({ imageUrl, width, chainId, assetAddress, assetId, 
         } else {
             navigate(`/moonsama/customizer/${chainId}/${assetAddress}/${assetId}`)
         }
-
     }
-    return (<CustomizerCard selected={false} imageUrl={imageUrl} width={width} height={height}>
+    return (<CustomizerCard selected={cardData.equipped} imageUrl={imageUrl} width={width} height={height}>
         <HStack spacing="0" w="100%" fontFamily="Rubik" fontSize="16px" lineHeight="24px" minHeight="24px" overflow="hidden" justifyContent="space-between">
             <HStack spacing="0" w="100%">
                 <Box w="100%" whiteSpace="nowrap" textOverflow="ellipsis">{traitValue}</Box>
@@ -49,11 +75,25 @@ const TraitCustomizerCard = ({ imageUrl, width, chainId, assetAddress, assetId, 
             </HStack>
         </HStack>
         <HStack spacing="0" w="100%">
-            {equipped
+            {cardData.equipped
                 ?
-                <Button flex="1" onClick={() => { }}>Remove</Button>
+                <Button flex="1" onClick={() => { }} isDisabled={cardData.equipped}>Equipped</Button>
                 :
-                <Button flex="1" onClick={() => { }}>Equip</Button>
+                <Button flex="1" onClick={() => {
+                    console.log("clicky")
+                    const currCustomization = selectCustomizerCustomizations(state.getState())?.find(c => c.parentChainId === parentChainId && c.parentAssetAddress === parentAssetAddress && c.parentAssetId === parentAssetId)
+                    if (!!currCustomization) {
+                        console.log("found current customization")
+                        const existingAsset = currCustomization.assets.find(a => a.chainId === chainId && a.assetAddress === assetAddress)
+                        if (!!existingAsset) {
+                            console.log("found existing asset")
+
+                            dispatch(removeCustomizerAssets({ parentChainId, parentAssetAddress, parentAssetId, assets: [existingAsset] }))
+                        }
+                        dispatch(addCustomizerAssets({ parentChainId, parentAssetAddress, parentAssetId, assets: [{ chainId, assetAddress, assetId }] }))
+
+                    }
+                }}>Equip</Button>
             }
         </HStack>
     </CustomizerCard>)
